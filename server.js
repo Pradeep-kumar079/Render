783b4bc95c330f7f00bc7fba0c16733f717b8a82
Pradeep-kumar079 <pradeepk9348@gmail.com>
2025-11-03 10:56:13 +0530

    ready

--- server.js
+++ server.js
@@ -36,10 +36,7 @@ app.use(
   })
 );
 
-// ✅ Handle preflight requests
-app.options("*", cors());
-
-// ✅ Serve static uploads
+app.options(/.*/, cors());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
 // ✅ MongoDB connection
